63ca19afcecd642db2178bf1b00b7b8fcae3fb9e
mgarmuno <miguelgarmm@gmail.com>
2021-02-23 19:10:14 +0100

    First commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mgarmuno/clipGo
+
+go 1.16
+
+require (
+	github.com/getlantern/golog v0.0.0-20201105130739-9586b8bde3a9 // indirect
+	github.com/getlantern/systray v0.0.0-20181206010516-eaad7114094d
+)
--- main.go
+++ main.go
@@ -1,8 +1,47 @@
 package main
 
-import "github.com/goki/gi/gi"
+import (
+	"bytes"
+	"image"
+	"image/png"
+	"log"
+	"os"
+
+	"github.com/getlantern/systray"
+)
 
 func main() {
-	win := gi.NewMainWindow("demo", "Demo", 600, 400)
-	win.StartEventLoop()
+	systray.Run(onReady, onExit)
+}
+
+func onReady() {
+	buf := getImageBytes()
+
+	systray.SetIcon(buf.Bytes())
+	systray.SetTitle("Joder")
+	systray.SetTooltip("A ver si podemos hacer esta mierda")
+}
+
+func onExit() {
+
+}
+
+func getImageBytes() *bytes.Buffer {
+	f, err := os.Open("icons/icon-white.png")
+	if err != nil {
+		log.Panic("Error reading icon file: ", err)
+	}
+
+	image, _, err := image.Decode(f)
+	if err != nil {
+		log.Panic("Error decoding icon: ", err)
+	}
+
+	buf := new(bytes.Buffer)
+	err = png.Encode(buf, image)
+	if err != nil {
+		log.Panic("Error encoding icon: ", err)
+	}
+
+	return buf
 }
